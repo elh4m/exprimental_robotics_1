fe1964d1a9cb76f7a215d9613366cad90d2467fa
Elham Mohammadi <77781922+elh4m@users.noreply.github.com>
2023-01-12 11:53:58 +0000

    Add files via upload

---
+++ doc_internal/CMakeFiles/docs_internal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/docs_internal"
+  "commands_history.md"
+  "tags_history.md"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/docs_internal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
