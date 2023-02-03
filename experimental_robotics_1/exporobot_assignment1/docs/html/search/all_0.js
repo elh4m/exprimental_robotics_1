c927c58f07db05cb2e513937f7d516d42f54940e
Elham Mohammadi <77781922+elh4m@users.noreply.github.com>
2023-02-03 14:24:06 +0000

    Delete experimental_robotics_1/exporobot_assignment1 directory

--- experimental_robotics_1/exporobot_assignment1/docs/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['project_201_20_2d_20experimental_20robotics_20_28msc_20robotics_20engineering_2c_20unige_29_0',['Project 1 - Experimental Robotics (MSc Robotics Engineering, Unige)',['../md_README.html',1,'']]]
-];
